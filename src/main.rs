8e4014b6a85d87d76f64176464d1cc106890cb81
Garvit <gaurgarvit64@gmail.com>
2024-08-31 01:17:06 +0530

    pass 1st stage

--- src/main.rs
+++ src/main.rs
@@ -21,7 +21,7 @@ fn main() {
                 String::new()
             });
 
-            Uncomment this block to pass the first stage
+            // Uncomment this block to pass the first stage
             if !file_contents.is_empty() {
                 panic!("Scanner not implemented");
             } else {
